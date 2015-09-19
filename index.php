e13e45cfa83df39b6011ab5f4f3f4c987502f64b
Neil Brayfield <neil@brayfield.uk>
2015-09-19 16:30:41 +0100

    allowed for environmental variable to add a module for testing without it needing to be in the config

--- index.php
+++ index.php
@@ -15,9 +15,18 @@ require_once DOCROOT.'vendor/autoload.php';
 // Initialize the framework
 $app = new Phalcana\Phalcana();
 
+$modules = array('unittest' => MODPATH.'unittest/');
+
+if (Phalcana\Phalcana::$mode == Phalcana\Phalcana::TESTING) {
+    // Check for module environment
+    if ($module = getenv('phalacana_module')) {
+        $modules[$module] = MODPATH.$module.'/';
+    }
+    unset($module);
+}
 
 // Instead of loading the main function for the app add the unit test module
-$app->fs->setModules(array('unittest' => MODPATH.'unittest/') + $app->fs->getModules());
+$app->fs->setModules($modules + $app->fs->getModules());
 
 // Fix issue with PHPUnit serialization of globals
-unset($app);
+unset($app, $modules);
